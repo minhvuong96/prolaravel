74b50cd5abef06aa7e151614bd4f2b395e26fbb9
Phạm Minh Vương <37030964+minhvuong96@users.noreply.github.com>
2018-10-23 21:40:02 +0700

    Đưa về code ban đầu

--- app/cate.php
+++ app/cate.php
@@ -8,7 +8,7 @@ class cate extends Model
 {
     protected $table = 'cates';
     protected $fillable = ['name','alias','order','parent_id','keywords','description'];
-    public $timestamps = true;
+    public $timestamps = false;
     public function product(){
     	return $this->hasMany('App\product');
     }
