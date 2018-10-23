c9eca861be6944abb9a8f88f4336cdcb017c8401
MinhVuong96 <vuongvmu96@gmail.com>
2018-10-23 21:37:42 +0700

    Vuong chinh sua cate model

--- app/cate.php
+++ app/cate.php
@@ -8,7 +8,7 @@ class cate extends Model
 {
     protected $table = 'cates';
     protected $fillable = ['name','alias','order','parent_id','keywords','description'];
-    public $timestamps = false;
+    public $timestamps = true;
     public function product(){
     	return $this->hasMany('App\product');
     }
